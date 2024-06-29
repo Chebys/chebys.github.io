b8d09f4da9d17f416ad698b69aeba8123b3d29d2
Chebys <Chebys@users.noreply.github.com>
2024-06-29 23:37:20 +0800

    Update kv.js

--- functions/kv.js
+++ functions/kv.js
@@ -1,7 +1,7 @@
 export async function onRequest(context){
 	const url=new URL(context.request.url);
 	const get=p=>url.searchParams.get(p)
-	const key=get('key'), mode=get('mode'), value, res
+	const key=get('key'), mode=get('mode'), value=null, res=null
 	if(mode=='set'){
 		value=context.request.text()||'asdasd';
 		res=(await context.env.KV.put(key, value))?'成功':'失败';
