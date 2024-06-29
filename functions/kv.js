8e7d60ffe50612f7ed9666d41c55d688088b85dd
Chebys <Chebys@users.noreply.github.com>
2024-06-29 23:26:42 +0800

    Update kv.js

--- functions/kv.js
+++ functions/kv.js
@@ -1,7 +1,13 @@
 export async function onRequest(context){
 	const url=new URL(context.request.url);
-	const k=url.searchParams.get('k')
-	const res=await context.env.KV.get('PRIME')
+	const get=p=>url.searchParams.get(p)
+	const key=get('key'), mode=get('mode'), value, res
+	if(mode=='set'){
+		value=context.request.text()||'asdasd';
+		res=(await context.env.KV.put(key, value))?'成功':'失败';
+	}else{
+		res=await context.env.KV.get('PRIME')
+	}
 	//return context.env.ASSETS.fetch('/The Corruption/')
 	return new Response(res)
 }
