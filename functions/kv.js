76b3bfff768433bd9f3951df72f73d7fc81a67c5
Chebys <Chebys@users.noreply.github.com>
2024-06-30 00:04:06 +0800

    Update kv.js

--- functions/kv.js
+++ functions/kv.js
@@ -2,12 +2,16 @@ export async function onRequest(context){
 	const url=new URL(context.request.url);
 	const get=p=>url.searchParams.get(p)
 	var key=get('key'), mode=get('mode'), value, res
-	if(mode=='set'){
+	if(!key){
+		res='缺少key';
+	}else if(mode=='set'){
 		value=await context.request.text()||'asdasd';
 		res=await context.env.KV.put(key, value)?'成功':'失败';
 	}else{
 		res=await context.env.KV.get('PRIME')
 	}
 	//return context.env.ASSETS.fetch('/The Corruption/')
-	return new Response(res)
+	var response=new Response(res)
+	response.headers.set('Access-Control-Allow-Origin', url.origin);
+	return response
 }
