0fa0af660a07d11706daea0894bbabec88111d43
Chebys <Chebys@users.noreply.github.com>
2024-06-30 22:40:45 +0800

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,7 +1,8 @@
 export async function onRequest(context){
 	const msg_h='chebys.pages.dev/proxy: ';
-	//const url=new URL(context.request.url);
-	//const get=p=>url.searchParams.get(p);
+	const url=new URL(context.request.url);
+	const get=p=>url.searchParams.get(p);
+	const mode=get('mode');
 	var tURL, res=null;
 	var target=context.request.url.match(/\?url=(.+)/)?.[1];
 	function error_res(msg){
@@ -13,9 +14,16 @@ export async function onRequest(context){
 	}else{
 		try{
 			tURL=new URL(target);
-			res=await fetch(tURL);
+			res=await fetch(tURL); //fetch失败不一定报错……
 			let headers=res.headers;
-			res=new Response(await res.blob());
+			if(mode=='complete-page'&&headers.get('Content-Type').match('text/html')){
+				let t=await res.text();
+				t=t.replace(/src="\/[^\/]/, `src="${tURL.origin}/`);
+				t=t.replace(/href="\/[^\/]/, `href="${tURL.origin}/`);
+				res=new Response(t)
+			}else{
+				res=new Response(await res.blob());
+			}
 			for(let [k,v] of headers)res.headers.set(k, v);
 		}catch(e){
 			if(!tURL)error_res('invalid url');
