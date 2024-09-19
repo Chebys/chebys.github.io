22dd29c9d2b67e7078bb2945f52777b553f67f74
Chebys <Chebys@users.noreply.github.com>
2024-09-19 18:42:22 +0800

    transferer

---
+++ functions/kv_transferer.js
@@ -0,0 +1,21 @@
+export async function onRequest(context){
+	const url=new URL(context.request.url);
+	const get=p=>url.searchParams.get(p)
+	const key=get('key'), mode=get('mode')
+	const KV=context.env.KV.transferer
+	var res
+	if(!key){
+		res='缺少key';
+	}else if(mode=='set'){
+		let value=await context.request.text();
+		res='succeeded';
+		await KV.put(key, value)
+			.catch(err=>{res='failed'})
+	}else{
+		res=await KV.get(key)
+	}
+	var response=new Response(res)
+	response.headers.set('Access-Control-Allow-Origin', '*');
+	response.headers.set('Cache-Control', 'no-store');
+	return response
+}
