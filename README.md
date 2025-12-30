5ee1c30131c2994336f7bb70679e98e880b52bc8
Chebys <chebyshevsky@gmail.com>
2025-12-30 14:09:00 +0800

    spa-sw.js

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # chebys.github.io
+- https://x-ze.cn/
+- https://chebys.pages.dev/
+- https://chebys.github.io/ （功能不完整）
 
-b站：<https://space.bilibili.com/289432465>
+子站点：
+- https://spa.x-ze.cn/
+- https://js.x-ze.cn/
+<!-- b站：<https://space.bilibili.com/289432465> -->
---
+++ js/spa-sw.js
@@ -0,0 +1,33 @@
+import IDBStorage from 'https://js.x-ze.cn/idb-storage'
+
+const ORIGIN = 'https://spa.x-ze.cn'
+const TTL = 1000*3600*24 //1 天
+const store = new IDBStorage('spa-frame', 'meta')
+
+async function refreshCache(){
+	let res = await fetch(ORIGIN)
+	let html = await res.text()
+	await store.set('src-cache', {
+		date: Date.now(),
+		html
+	})
+}
+async function cacheFirst(ev){
+	let {date, html} = await store.get('src-cache')
+	if(Date.now()-date > TTL)
+		refreshCache()
+	else
+		ev.respondWith(new Response(html, { 'Content-Type': 'text/html; charset=utf-8' })) 
+}
+
+self.addEventListener('install', ev => ev.waitUntil(refreshCache()))
+
+self.addEventListener('fetch', ev => {
+  const url = new URL(ev.request.url);
+  if(url.origin != ORIGIN)
+	  return
+  
+  if (url.pathname == '/' || url.pathname.startsWith('/app/')) {
+    cacheFirst(ev)
+  }
+})
